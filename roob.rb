729644e5b52fae8e67050d005db5dc684c2129cb
Bill Patrianakos <bill@chooseclever.com>
2013-01-20 14:21:06 -0600

    First commit

---
+++ roob.rb
@@ -0,0 +1,37 @@
+#! /usr/bin/env ruby
+require 'aws-sdk'
+
+bucket = ARGV[0]
+file = ARGV[1]
+path = ARGV[2]
+
+unless bucket && file
+	puts "Usage: uppit <BUCKET_NAME> <FILE_NAME>"
+	exit 1
+end
+
+unless path
+	path = ''
+end
+
+s3 = AWS::S3.new(
+	:access_key_id => 'AKIAJ3YMT6TGLY7YUW6A',
+	:secret_access_key => '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')
+
+# Create a bucket
+
+# Use existing bucket
+bucket = s3.buckets[bucket]
+
+basename = File.basename(file)
+upload = bucket.objects["#{path}/#{basename}"]
+upload.write(:file => file)
+puts "Uploaded #{file} to: "
+puts upload.public_url
+
+# List buckets
+# s3.buckets.each do |bucket|
+#	puts bucket.name
+# end
+
+exit 0
