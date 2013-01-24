207149f31d3b6e70cbbde7cf7d0039cfb2874689
Bill Patrianakos <bill@chooseclever.com>
2013-01-24 15:54:07 -0600

    Messing with optparse

--- script/roob.rb
+++ script/roob.rb
@@ -1,5 +1,38 @@
 #! /usr/bin/env ruby
+require 'optparse'
 require 'aws-sdk'
+
+#--- Options ---#
+options = {}
+
+opt_parser = OptionParser.new do |opt|
+	opt.banner 		= "Usage: sploder <COMMAND> [OPTIONS]"
+	opt.separator 	""
+	opt.separator 	"Commands"
+	opt.separator 	"  upload: Upload a file to S3"
+	opt.separator 	"    Usage: sploder upload [BUCKET_NAME] [FILE] Optional: [BUCKET_PATH] [ACL]"
+	opt.separator 	"  create: Creates a new S3 bucket"
+	opt.separator 	"    Usage: sploder create [BUCKET_NAME]"
+	opt.separator 	"  list: Lists your buckets and files"
+	opt.separator 	""
+	opt.separator 	"Options"
+
+	opt.on("-e","--environment ENVIRONMENT","which environment you want server run") do |environment|
+		options[:environment] = environment
+	end
+
+	opt.on("-d","--daemon","runing on daemon mode?") do
+		options[:daemon] = true
+	end
+
+	opt.on("-h","--help","help") do
+		puts opt_parser
+	end
+end
+
+opt_parser.parse!
+
+puts opt_parser
 
 bucket = ARGV[0]
 file = ARGV[1]
@@ -8,12 +41,12 @@ path = ARGV[2]
 # TODO: Add ACL
 # Canned ACLs:
 # private, public_read, public_read_write, authentication_read, bucket_owner_read,
-
+=begin
 unless bucket && file
 	puts "Usage: uppit <BUCKET_NAME> <FILE_NAME>"
 	exit 1
 end
-
+=end
 unless path
 	path = ''
 end
